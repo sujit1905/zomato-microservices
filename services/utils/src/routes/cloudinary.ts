a69aa0858eba86bdaa9e9d7106698f8cd4f5a865
Sujit Mecwan <mecwansujit@gmail.com>
2026-07-25 11:23:18 +0530

    Add cloudinary debug logs

--- services/utils/src/routes/cloudinary.ts
+++ services/utils/src/routes/cloudinary.ts
@@ -6,14 +6,22 @@ const router = express.Router();
 router.post("/upload", async (req, res) => {
   try {
     const { buffer } = req.body;
+
+    console.log("Buffer starts with:", buffer?.substring(0, 50));
+
     const cloud = await cloudinary.v2.uploader.upload(buffer);
 
     res.json({
       url: cloud.secure_url,
     });
   } catch (error: any) {
+    console.error("FULL CLOUDINARY ERROR:");
+    console.error(error);
+
     res.status(500).json({
       message: error.message,
+      name: error.name,
+      http_code: error.http_code,
     });
   }
 });
